e1e5cb90074f6f4cc0922dd87dd359679c59856f
counttoten <sejin1314@korea.ac.kr>
2024-08-27 09:50:36 +0900

    refactor: newsletter_id 필드 삭제

--- supabase/functions/api/models/entities/article.ts
+++ supabase/functions/api/models/entities/article.ts
@@ -1,7 +1,6 @@
 export interface Article {
   id: string;
   to_user_id: string;
-  newsletter_id: string | null;
   from_name: string | null;
   from_domain: string;
   title: string;
--- supabase/functions/api/models/entities/subscription.ts
+++ supabase/functions/api/models/entities/subscription.ts
@@ -1,7 +1,6 @@
 export interface Subscription {
   id: string;
   user_id: string;
-  newsletter_id: string | null;
   newsletter_name: string;
   newsletter_domain: string;
   created_at: string;
