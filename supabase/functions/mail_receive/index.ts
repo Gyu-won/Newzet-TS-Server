d0ea50e50d4384bcc24fb54f5991c5107ad6c06d
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-08-27 20:58:24 +0900

    [NZ-296] feat: 메일 데이터에서 fromName, fromEmail, to를 추출하는 코드 추가

--- supabase/functions/mail_receive/index.ts
+++ supabase/functions/mail_receive/index.ts
@@ -16,6 +16,11 @@ Deno.serve(async (req: Request): Promise<Response> => {
   try {
     const { bucketName, objectKey } = await req.json();
     const responseBody = await getObjectFromS3(bucketName, objectKey);
+    const { fromName, fromEmail, to } = await parseEmailContent(responseBody);
+
+    console.log('From Name:', fromName);
+    console.log('From Email:', fromEmail);
+    console.log('To:', to);
 
     return new Response(JSON.stringify({ status: 'success' }), { status: 200 });
   } catch (error) {
@@ -37,4 +42,47 @@ async function getObjectFromS3(bucketName: string, objectKey: string) {
   }
 
   return response.Body;
+}
+
+async function parseEmailContent(responseBody: ReadableStream<Uint8Array>) {
+  const emailContent = new TextDecoder('utf-8').decode(
+    await new Response(responseBody).arrayBuffer(),
+  );
+
+  const mimeHeaderMatch = emailContent.match(/MIME-Version:.*?\r?\n\r?\n/s);
+  if (!mimeHeaderMatch) {
+    throw new Error('S3 MIME 데이터 타입 에러');
+  }
+
+  const headersSection = mimeHeaderMatch[0].trim();
+  const headerLines = headersSection.split(/\r?\n/);
+
+  let from = '';
+  let to = '';
+
+  headerLines.forEach((line) => {
+    if (line.startsWith('From:')) {
+      from = line.replace('From:', '').trim();
+    } else if (line.startsWith('To:')) {
+      to = line.replace('To:', '').trim();
+    }
+  });
+
+  let [fromName, fromEmail] = from.split(/(?=\s<)/);
+  fromName = decodeBase64(fromName);
+  fromEmail = fromEmail ? fromEmail.replace(/[<>]/g, '').trim() : '';
+
+  return { fromName, fromEmail, to };
+}
+
+function decodeBase64(encodedText: string) {
+  const match = encodedText.match(/=\?UTF-8\?B\?(.+)\?=/);
+  if (match) {
+    const base64text = match[1];
+    const decodedText = atob(base64text);
+    return new TextDecoder('utf-8').decode(
+      new Uint8Array([...decodedText].map((char) => char.charCodeAt(0))),
+    );
+  }
+  return encodedText;
 }
