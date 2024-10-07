6efb1487c8c5e9e8c3539fdac8950c980d7f52fd
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-10-07 18:26:16 +0900

    [NZ-385] feat: 웰컴메일 아티클 추가, 구독 목록 추가, 알림 전송 로직 구현

---
+++ supabase/functions/api/services/welcomeService.ts
@@ -0,0 +1,50 @@
+import { ArticleRepository } from '../repositories/articleRepository.ts';
+import { FcmNotificationsRepository } from '../repositories/fcmNotificationRepository.ts';
+import { NewsletterRepository } from '../repositories/newsletterRepository.ts';
+import { SubscriptionRepository } from '../repositories/subscriptionRepository.ts';
+import { UserinfoRepository } from '../repositories/userinfoRepository.ts';
+
+export class WelcomeService {
+  private userinfoRepository: UserinfoRepository;
+  private newsletterRepository: NewsletterRepository;
+  private articleRepository: ArticleRepository;
+  private subscriptionRepository: SubscriptionRepository;
+  private fcmNotificationRepository: FcmNotificationsRepository;
+
+  constructor() {
+    this.userinfoRepository = new UserinfoRepository();
+    this.newsletterRepository = new NewsletterRepository();
+    this.articleRepository = new ArticleRepository();
+    this.subscriptionRepository = new SubscriptionRepository();
+    this.fcmNotificationRepository = new FcmNotificationsRepository();
+  }
+
+  async sendWelcomeMail(userId: string) {
+    const userinfo = await this.userinfoRepository.getUserinfo(userId);
+    const newsletter = await this.newsletterRepository.getNewsletterById(
+      'c4922e54-f58a-4270-80da-2dc6d59bc4fa',
+    );
+    const article = await this.articleRepository.addArticle(
+      userinfo.id,
+      newsletter.name,
+      newsletter.domain,
+      '💌 뉴젯과 더욱 친해지는 방법 💌',
+      '7oskhlv05baub38dmatd0njp8qjgjibk0bo6uk81.html',
+      newsletter.mailling_list,
+    );
+    await this.subscriptionRepository.addSubscription(
+      userinfo.id,
+      newsletter.name,
+      newsletter.domain,
+      newsletter.mailling_list,
+    );
+
+    await this.fcmNotificationRepository.addFcmNotification(
+      userinfo.id,
+      article.id,
+      article.title,
+      article.from_name,
+      article.created_at,
+    );
+  }
+}
