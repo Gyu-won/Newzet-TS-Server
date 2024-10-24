ebc7c9bb1f11ebf8e4c0fe3952cfb1e6b801063d
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-10-24 14:11:54 +0900

    [NZ-422] feat: article 내용 조회 시 is_like 값도 반환하도록 코드 변경

--- supabase/functions/api/models/dtos/article/articleContentResDto.ts
+++ supabase/functions/api/models/dtos/article/articleContentResDto.ts
@@ -1,9 +1,11 @@
 export class ArticleContentResDto {
   title: string;
   content: string;
+  isLike: boolean;
 
-  constructor(title: string, content: string) {
+  constructor(title: string, content: string, isLike: boolean) {
     this.title = title;
     this.content = content;
+    this.isLike = isLike;
   }
 }
