6b44257e64f05623bb1abb0c541c202d3742d8f1
counttoten <sejin1314@korea.ac.kr>
2024-10-29 12:48:23 +0900

    [NZ-433] docs: 구독 조회 API 관련 주석 추가

--- supabase/functions/api/routers/subscriptionRouter.ts
+++ supabase/functions/api/routers/subscriptionRouter.ts
@@ -7,8 +7,14 @@ const subscriptionController = new SubscriptionController();
 
 subscriptionRouter.use(authMiddleware);
 
+// 구독 목록 조회 API
+/// v1: GET /subscription
+/// get_subscription_list_with_image RPC 호출
+/// v2: GET /subscription/list
+/// get_subscription_with_image RPC 호출
 subscriptionRouter.get('', (c) => subscriptionController.getSubscriptionListV1(c));
 subscriptionRouter.get('/list', (c) => subscriptionController.getSubscriptionListV2(c));
+
 subscriptionRouter.delete('/:subscriptionId', (c) =>
   subscriptionController.deleteSubscriptionV1(c),
 );
